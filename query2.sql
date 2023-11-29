d494463a7cec37d863d5a38ec58c58fe0b578ba9
Jonathan <jonkar2@kth.se>
2023-11-29 19:07:51 +0100

    fixed

--- query2.sql
+++ query2.sql
@@ -2,11 +2,27 @@ SET SEARCH_PATH = seminar;
 
 
 Select  "sb_count" as "No of Siblings", COUNT("id") as "No of Students"  FROM
-(SELECT count(s.student_id) as "sb_count", s.student_id as "id"
-FROM student as s
-JOIN sibling_of_student as sb
-ON s.student_id = sb.student_id
-GROUP by s.student_id)
+(
+select id,sum(sb_count) as "sb_count" from (
+
+  select sb_count,id from (SELECT count(sb.student_id) as "sb_count", s.student_id as "id"
+	FROM student as s
+	FULL JOIN sibling_of_student as sb
+	ON s.student_id = sb.sibling_id
+	GROUP by s.student_id
+	ORDER BY id)
+  union all
+  select sb_count,id from (SELECT count(sb.student_id) as "sb_count", s.student_id as "id"
+	FROM student as s
+	FULL JOIN sibling_of_student as sb
+	ON s.student_id = sb.student_id
+	GROUP by s.student_id
+	ORDER BY id
+	)
+
+
+)
+	group by id)
 GROUP by "No of Siblings"
 
 ORDER BY "No of Siblings"
