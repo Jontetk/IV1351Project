c77b18fe98e4bac0b4bfa49ffa515258256ec621
bhnur <bhnur@kth.se>
2023-12-12 17:41:22 +0100

    did comments for documentation 📄📄

--- sem4/src/main/java/se/kth/iv1351/sem4/model/Student.java
+++ sem4/src/main/java/se/kth/iv1351/sem4/model/Student.java
@@ -1,21 +1,24 @@
 package se.kth.iv1351.sem4.model;
 
-
-
-import java.sql.*;
 import java.util.*;
+
 
 public class Student {
 
     private int id;
     private String personNumber;
     
-    
+    /**
+     * An object representing a student
+     * @param id of the student 
+     * @param personNumber of the student 
+     */
     public Student(int id, String personNumber) {
         this.id = id;
         this.personNumber = personNumber;
     }
 
+   
     public int getId() {
         return id;
     }
@@ -25,6 +28,12 @@ public class Student {
     }
 
 
+    /**
+     * Checks if the rental is allowed to be terminated.
+     * @param rentals a list of all rentals that has been done by this student
+     * @param rentalId id of the rental to be terminated
+     * @return true if the rental to be terminated both exists in rentals and rental is ongoing (no end date) 
+     */
     public boolean rentalCanBeTerminated(ArrayList<RentalDTO> rentals, int rentalId) {
         for(RentalDTO rental : rentals) {
             if(rentalId == rental.getRentalId() && rental.getEndDate() == null)
@@ -34,6 +43,15 @@ public class Student {
 
     }
     
+    /**
+     * Creates and rental object representing the rental made by student
+     * @param rentals a list of all rentals in the model
+     * @param availibleInstruments all instruments availible for rental
+     * @param instrumentId id of the instrument to rent
+     * @param maxRental the maximum amount of rental that a student can make
+     * @return a rental object {@link RentalDTO} representing the rental made by student
+     * @throws RentalException
+     */
     public RentalDTO rentInstrument(ArrayList<RentalDTO> rentals, ArrayList<InstrumentDTO> availibleInstruments, int instrumentId,int maxRental)
     throws RentalException {
     
@@ -50,19 +68,30 @@ public class Student {
 
     }
 
-    
+    /**
+     * Checks wheter the rental can be made 
+     * @param rentals the list of all rentals in model
+     * @param rentalLimit the max allowed ongoing rentals
+     * @return true if student's ongoing rental count is below the limit. Returns false otherwise
+     */
     private boolean isRentalAllowed(ArrayList<RentalDTO> rentals, int rentalLimit){
 
-        // rental count logic. May need to refactor into if rental valid function
+        
         int totalRentals = 0;
         for(RentalDTO rental:rentals) {
             if(rental.getStudentId() == this.id && rental.getEndDate() == null)
                 totalRentals += 1;
         }
-        // check if limit > total and then returns bool
+       
         return  rentalLimit > totalRentals;
     }
 
+    /**
+     * Checks wheter the instrument with a certain id is avalible to rent
+     * @param availibleInstruments a list of all instruments avalible to rent
+     * @param instrumentId id of the instrument that is being selected
+     * @return true if an avalible instrument with the {@code instrumentId}
+     */
     private boolean isSelectedInstrumentAvalible(ArrayList<InstrumentDTO> availibleInstruments, int instrumentId){
         Boolean isAvalible = false;
         for(InstrumentDTO ins: availibleInstruments){
@@ -73,7 +102,5 @@ public class Student {
         }
         return isAvalible;
     }
-
-    //cancel rental
 
 }
